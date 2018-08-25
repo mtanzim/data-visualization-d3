21aed4630e8f8f34288e658676a24dc59a020971
Tanzim Mokammel <mtanzim@gmail.com>
2018-08-25 17:28:53 +0800

    added 1st demo and bar chart

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Learning D3.js
 
-[Video Tutorial](https://www.youtube.com/watch?v=C4t6qfHZ6Tw&t=1400s)
+- [Video Tutorial](https://www.youtube.com/watch?v=C4t6qfHZ6Tw&t=1400s)
+- [Interactive Tutorial](https://scrimba.com/g/gd3js)
+- [Documentation](https://d3js.org/)
 
 ## Lessons Learned
---
+++ public/index.js
@@ -0,0 +1,35 @@
+var dataset = ['this', 'is', 'a', 'test'];
+
+d3.select('#intro')
+  .selectAll('h4')
+  .data(dataset)
+  // takes data items and performs further operations on it
+  .enter()
+  .append('h4') // appends paragraph for each data element
+  // .text('D3 is awesome!!');
+  .text(d => d)
+  .style("padding-left", (d,i) => `${i*20+50}px`)
+  .attr("class","text-danger");
+
+var datasetBarChart = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
+
+var svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var barChart = svg.selectAll("rect")
+  .data(datasetBarChart)
+  // takes data from waiting state to 
+  .enter()
+  // below operations get applied to 
+  .append("rect")
+  .attr("fill", (d,i) => i % 2 === 0 ? "salmon" : "cyan")
+  .attr("y", d=> svgHeight - d )
+  .attr("height", d=> d )
+  .attr("width", barWidth - barPadding )
+  .attr("transform", (d,i) =>{
+    var translate = [barWidth * i, 0];
+    return `translate(${translate})`;
+  } )
