0f87482fc6ac6843a82f2c7314758550658ce023
Tanzim Mokammel <mtanzim@gmail.com>
2018-08-25 17:48:51 +0800

    fixed bar chart bug

--- public/index.js
+++ public/index.js
@@ -13,7 +13,8 @@ d3.select('#intro')
 
 var datasetBarChart = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
+var barWidth = (svgWidth / datasetBarChart.length);
+console.log(barWidth);
 
 var svg = d3.select('svg')
   .attr("width", svgWidth)
